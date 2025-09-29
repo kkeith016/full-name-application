8703a9216ab6f6d2a8dfa921e65a732fb5129ce5
Kylee <kkeith.biz@gmail.com>
2025-09-29 10:59:53 -0400

    Fullname finished step one.

---
+++ src/main/java/com/pluralsight/fullNameApp.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class fullNameApp {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String firstName = keyboard.nextLine();
+
+        System.out.print("Enter your middle initial: ");
+        char middleInitial = keyboard.next().charAt(0);
+        keyboard.nextLine();
+
+        System.out.print("Enter your last name: ");
+        String lastName = keyboard.nextLine();
+
+        System.out.print("Enter any suffix if none leave blank: ");
+        String Suffix =  keyboard.nextLine();
+
+       System.out.println("Analyzing");
+        System.out.println(firstName + " " + middleInitial + " " + lastName + " Welcome to the Matrix");
+
+    }
+}
